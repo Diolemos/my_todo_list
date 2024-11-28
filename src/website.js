37a95fe13c577238cbb40256cb0a16198a0ec4db
Dio Lemos <pedro.devsc@gmail.com>
2024-11-28 11:46:18 -0300

    update website.js create createNavBtn()

--- src/website.js
+++ src/website.js
@@ -33,32 +33,35 @@ function createHeader() {
   
   function createNav() {
     const nav = document.createElement("nav");
-    //Now each button should be created individually, starting from the default btn
-    const defaultButton = document.createElement("button");
-    defaultButton.classList.add("button-nav");
-    defaultButton.textContent = "Default";
-  
-    //Event listener... If user is not on the clicked option, take him there!
-    defaultButton.addEventListener("click", (e) => {
-      if (e.target.classList.contains("active")) return;
-      //setActiveButton(this);
-      //run the upcoming function 'setActiveBtn()'
-      //for now, there will only be one button
-      //Load Project()
-    });
-    nav.appendChild(defaultButton);
     return(nav)
 }
   
    //Navigation Items/btns will be dynamically created/deleted 
   
+    function createNavBtn(btnName){
+    const button = document.createElement("button");
+    button.classList.add("button-nav", `button-${btnName.toLowerCase()}` );
+    button.textContent = btnName;
     
+  
+    
+    button.addEventListener("click", (e) => {
+      if (e.target.classList.contains("active")) return;
+      //setActiveButton(e.target); //
+      //TODO run the upcoming function 'setActiveBtn()'
+      //for now, there will only be one button
+      //TODO LoadProject()
+    });
+    return button;
+    }
   
 
 function initializeWebsite(){
     const body = document.body;
     body.appendChild(createHeader());
     body.appendChild(createSideBar());
+    const nav = document.querySelector("nav");
+    nav.appendChild(createNavBtn("Default"));
     body.appendChild(createMain());
    
 }
